360063bc08e5e9d5946181dc61a919e52e900129
Jose Zamariola <zamariolajr@gmail.com>
2021-09-07 08:38:44 -0300

    Inclusão de evento no menu para abrir e fechar

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 //adicionando interatividade ao menu
+// Abre e fecha menu quando ocorre evento click no hamburguer ou no x
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
     element.addEventListener('click', function() {
         nav.classList.toggle('show')
+    })
+}
+// fecha menu quando clica em algum item
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+    link.addEventListener('click', function() {
+        nav.classList.remove('show')
     })
 }
