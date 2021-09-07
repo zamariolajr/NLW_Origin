2d870adb7c64f122f827b86549630a57fd0167de
Jose Zamariola <zamariolajr@gmail.com>
2021-09-06 22:24:54 -0300

    update de arquivos

---
+++ main.js
@@ -0,0 +1,9 @@
+//adicionando interatividade ao menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+    element.addEventListener('click', function() {
+        nav.classList.toggle('show')
+    })
+}
